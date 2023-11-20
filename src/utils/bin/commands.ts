eb88ecb7fc8e39b92732ad5e9306b3a8911d7d85
Nyanto <ola.mushroom@gmail.com>
2023-11-20 15:31:09 +0700

    commit

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -2,13 +2,14 @@
 
 import * as bin from './index';
 import config from '../../../config.json';
+import { DateTime } from "luxon";
 
 // Help
 export const help = async (args: string[]): Promise<string> => {
   const commands = Object.keys(bin).sort().join(', ');
   var c = '';
   for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
-    if (i % 7 === 0) {
+    if (i % 10 === 0) {
       c += Object.keys(bin).sort()[i - 1] + '\n';
     } else {
       c += Object.keys(bin).sort()[i - 1] + ' ';
@@ -28,76 +29,22 @@ export const repo = async (args: string[]): Promise<string> => {
   return 'Opening Github repository...';
 };
 
-// About
-export const about = async (args: string[]): Promise<string> => {
-  return `Hi, I am ${config.name}. 
-Welcome to my website!
-More about me:
-'sumfetch' - short summary.
-'resume' - my latest resume.
-'readme' - my github readme.`;
-};
-
-export const resume = async (args: string[]): Promise<string> => {
-  window.open(`${config.resume_url}`);
-  return 'Opening resume...';
-};
-
 // Donate
 export const donate = async (args: string[]): Promise<string> => {
-  return `thank you for your interest. 
-here are the ways you can support my work:
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
+  return `
+- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.buymeacoffee}" target="_blank">buymeacoffe</a></u>
 `;
 };
 
 // Contact
-export const email = async (args: string[]): Promise<string> => {
-  window.open(`mailto:${config.email}`);
-  return `Opening mailto:${config.email}...`;
-};
-
 export const github = async (args: string[]): Promise<string> => {
   window.open(`https://github.com/${config.social.github}/`);
-
   return 'Opening github...';
-};
-
-export const linkedin = async (args: string[]): Promise<string> => {
-  window.open(`https://www.linkedin.com/in/${config.social.linkedin}/`);
-
-  return 'Opening linkedin...';
-};
-
-// Search
-export const google = async (args: string[]): Promise<string> => {
-  window.open(`https://google.com/search?q=${args.join(' ')}`);
-  return `Searching google for ${args.join(' ')}...`;
-};
-
-export const duckduckgo = async (args: string[]): Promise<string> => {
-  window.open(`https://duckduckgo.com/?q=${args.join(' ')}`);
-  return `Searching duckduckgo for ${args.join(' ')}...`;
-};
-
-export const bing = async (args: string[]): Promise<string> => {
-  window.open(`https://bing.com/search?q=${args.join(' ')}`);
-  return `Wow, really? You are using bing for ${args.join(' ')}?`;
-};
-
-export const reddit = async (args: string[]): Promise<string> => {
-  window.open(`https://www.reddit.com/search/?q=${args.join(' ')}`);
-  return `Searching reddit for ${args.join(' ')}...`;
 };
 
 // Typical linux commands
 export const echo = async (args: string[]): Promise<string> => {
   return args.join(' ');
-};
-
-export const whoami = async (args: string[]): Promise<string> => {
-  return `${config.ps1_username}`;
 };
 
 export const ls = async (args: string[]): Promise<string> => {
@@ -113,8 +60,24 @@ export const cd = async (args: string[]): Promise<string> => {
 if you want to help, you can type 'donate'.`;
 };
 
-export const date = async (args: string[]): Promise<string> => {
-  return new Date().toString();
+export const birthday = async (args: string[]): Promise<string> => {
+  const dt = DateTime.now();
+  const bday = DateTime.fromISO(`${dt.year}-12-02T08:00+07:00`);
+  const obj = bday.diff(dt, ['months', 'days', 'hours', 'minutes']).toObject();
+  let duration = "";
+  for (const i in obj) {
+    const val = obj[i];
+    if (val !== 0) {
+      const unit = val === 1 ? i.substring(0, i.length - 1) : i;
+      duration += `${Math.round(val)} ${unit} `;
+    }
+  }
+
+  return `
+    ${bday.toFormat("cccc',' dd LLLL y")}
+    ${bday.toFormat("HH':'mm ZZZZZ '('ZZ')'")}
+    In ${duration}
+  `;
 };
 
 export const vi = async (args: string[]): Promise<string> => {
@@ -141,15 +104,6 @@ export const sudo = async (args?: string[]): Promise<string> => {
 // Banner
 export const banner = (args?: string[]): string => {
   return `
-█████        ███                       ███████████                                   
-░░███        ░░░                       ░█░░░███░░░█                                   
- ░███        ████  █████ █████  ██████ ░   ░███  ░   ██████  ████████  █████████████  
- ░███       ░░███ ░░███ ░░███  ███░░███    ░███     ███░░███░░███░░███░░███░░███░░███ 
- ░███        ░███  ░███  ░███ ░███████     ░███    ░███████  ░███ ░░░  ░███ ░███ ░███ 
- ░███      █ ░███  ░░███ ███  ░███░░░      ░███    ░███░░░   ░███      ░███ ░███ ░███ 
- ███████████ █████  ░░█████   ░░██████     █████   ░░██████  █████     █████░███ █████
-░░░░░░░░░░░ ░░░░░    ░░░░░     ░░░░░░     ░░░░░     ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ 
-
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
