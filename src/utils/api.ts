d1d1d1522029dc136169128ef97dc3bb18a71d2f
Nyanto <ola.mushroom@gmail.com>
2023-11-20 16:24:33 +0700

    fix

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
 import config from '../../config.json';
 
-export const getProjects = async () => {
-  const { data } = await axios.get(
-    `https://api.github.com/users/${config.social.github}/repos`,
-  );
-  return data;
+export const getGitHubData = async () => {
+  return JSON.stringify(await axios.get(
+    `https://api.github.com/users/${config.social.github}`))
 };
 
 export const getWeather = async (city: string) => {
--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -1,28 +1,17 @@
 // // List of commands that require API calls
 
-import { getProjects } from '../api';
+import { getGitHubData } from '../api';
 import { getQuote } from '../api';
 import { getWeather } from '../api';
 
-export const projects = async (args: string[]): Promise<string> => {
-  const projects = await getProjects();
-  return projects
-    .map(
-      (repo) =>
-        `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
-    )
-    .join('\n');
+export const githubdata = async (args: string[]): Promise<string> => {
+  const data = await getGitHubData();
+  return data;
 };
 
 export const quote = async (args: string[]): Promise<string> => {
   const data = await getQuote();
   return data.quote;
-};
-
-export const readme = async (args: string[]): Promise<string> => {
-  const readme = await getReadme();
-  return `Opening GitHub README...\n
-  ${readme}`;
 };
 
 export const weather = async (args: string[]): Promise<string> => {
