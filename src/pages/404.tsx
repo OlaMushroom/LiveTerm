aeaf73fe321993cb0405c4c5346d23a12fc73bd5
Nyanto <ola.mushroom@gmail.com>
2024-08-03 22:51:50 +0700

    fix

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 const NotFoundPage = () => {
   const router = useRouter();
   React.useEffect(() => {
     router.replace('/');
   });
-  return <SpeedInsights route={router.pathname} />;
+  return null;
 };
 export default NotFoundPage;
